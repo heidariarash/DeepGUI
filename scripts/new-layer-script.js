1d23fd03061e0c9d706e2451d54a79f0dafee261
heidariarash <arashheidari94@outlook.com>
2020-10-03 18:09:15 +0330

    new layer window finished

--- scripts/new-layer-script.js
+++ scripts/new-layer-script.js
@@ -2,7 +2,13 @@ const electron = require('electron');
 const {ipcRenderer} = electron;
 
 document.getElementById('done').addEventListener('click', () => {
-    ipcRenderer.send('add-new-layer');
+    let new_layer = document.getElementById('layer-selector').value
+    if (['Convolution 1D', 'Convolution 2D', 'Convolution 3D', 'Max Pool 1D', 'Max Pool 2D','Max Pool 3D','Activation', 'Softmax'].indexOf(new_layer) >= 0) {
+        ipcRenderer.send('add-new-layer', new_layer);
+    }
+    else{
+        document.getElementById('new-layer-attention').setAttribute('style','opacity: 1');
+    }
 });
 
 document.getElementById('cancle').addEventListener('click', () => {
