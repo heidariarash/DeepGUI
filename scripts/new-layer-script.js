f7c50befb9eed5dee18766dd0f147e9ecc7ca931
heidariarash <arashheidari94@outlook.com>
2020-10-06 18:14:11 +0330

    deleting softmax from layer types

--- scripts/new-layer-script.js
+++ scripts/new-layer-script.js
@@ -3,7 +3,7 @@ const {ipcRenderer} = electron;
 
 document.getElementById('done').addEventListener('click', () => {
     let new_layer = document.getElementById('layer-selector').value
-    if (['Linear', 'Convolution 1D', 'Convolution 2D', 'Convolution 3D', 'Max Pool 1D', 'Max Pool 2D','Max Pool 3D','Activation', 'Softmax'].indexOf(new_layer) >= 0) {
+    if (['Linear', 'Convolution 1D', 'Convolution 2D', 'Convolution 3D', 'Max Pool 1D', 'Max Pool 2D','Max Pool 3D','Activation'].indexOf(new_layer) >= 0) {
         ipcRenderer.send('add-new-layer', new_layer);
     }
     else{
