8f05a653fd32c39d8f685b4669a2d2eed6327643
heidariarash <arashheidari94@outlook.com>
2020-10-08 18:55:35 +0330

    adding all loss functions

--- scripts/generate.js
+++ scripts/generate.js
@@ -10,42 +10,22 @@ generate_code = (options, path) => {
             switch(layer.name){
                 //linear case
                 case "Linear":
-                    if(layer.activation === "No Activation"){
-                        stw += `model.add(keras.layers.Dense(${layer.unit_num}))`
-                    }
-                    else {
-                        stw += `model.add(keras.layers.Dense(${layer.unit_num}, activation = '${layer.activation}'))`
-                    }
+                    stw += `model.add(keras.layers.Dense(${layer.unit_num}, activation = '${layer.activation.toLowerCase()}'))`
                     break;
 
                 //convolution 1D case
                 case "Convolution 1D":
-                    if(layer.activation === "No Activation"){
-                        stw += `model.add(keras.layers.Conv1D(${layer.filter_num}, ${layer.filter_size}, strides = ${layer.stride})`
-                    }
-                    else {
-                        stw += `model.add(keras.layers.Conv1D(${layer.filter_num}, ${layer.filter_size}, strides = ${layer.stride}, activation = '${layer.activation}')`
-                    }
+                    stw += `model.add(keras.layers.Conv1D(${layer.filter_num}, ${layer.filter_size}, strides = ${layer.stride}, activation = '${layer.activation.toLowerCase()}')`
                     break;
 
                 //convolution 2D case
                 case "Convolution 2D":
-                    if(layer.activation === "No Activation"){
-                        stw += `model.add(keras.layers.Conv2D(${layer.filter_num}, (${layer.filter_size[0]}, ${layer.filter_size[1]}), strides = ${layer.stride})`
-                    }
-                    else {
-                        stw += `model.add(keras.layers.Conv2D(${layer.filter_num}, (${layer.filter_size[0]}, ${layer.filter_size[1]}), strides = ${layer.stride}, activation = '${layer.activation}')`
-                    }
+                    stw += `model.add(keras.layers.Conv2D(${layer.filter_num}, (${layer.filter_size[0]}, ${layer.filter_size[1]}), strides = ${layer.stride}, activation = '${layer.activation.toLowerCase()}')`
                     break;
 
                 //convolution 3D case
                 case "Convolution 3D":
-                    if(layer.activation === "No Activation"){
-                        stw += `model.add(keras.layers.Conv3D(${layer.filter_num}, (${layer.filter_size[0]}, ${layer.filter_size[1]}, ${layer.filter_size[2]}), strides = ${layer.stride})`
-                    }
-                    else {
-                        stw += `model.add(keras.layers.Conv3D(${layer.filter_num}, (${layer.filter_size[0]}, ${layer.filter_size[1]}, ${layer.filter_size[2]}), strides = ${layer.stride}, activation = '${layer.activation})'`
-                    }
+                    stw += `model.add(keras.layers.Conv3D(${layer.filter_num}, (${layer.filter_size[0]}, ${layer.filter_size[1]}, ${layer.filter_size[2]}), strides = ${layer.stride}, activation = '${layer.activation.toLowerCase()})'`
                     break;
 
                 //max pool 1D case
