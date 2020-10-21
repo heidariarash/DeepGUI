14458eb6c48b896542e2a2e773581eeb00b36920
heidariarash <arashheidari94@outlook.com>
2020-10-21 18:42:38 +0330

    changing dense configure for PyTorch

--- scripts/dense-config.js
+++ scripts/dense-config.js
@@ -16,10 +16,37 @@ document.getElementById('done').addEventListener('click', ()=> {
     ipcRenderer.send("layer-config-finish", layer);
 });
 
-ipcRenderer.on("layer-config", (event , layer_config) => {
+ipcRenderer.on("layer-config", (event , arg) => {
+    layer_config = arg.layer;
     document.getElementById("number-of-units").value = layer_config.unit_num;
-    document.getElementById("activation-selector").value = layer_config.activation;
     layer = layer_config;
+    if (arg.framework == "PyTorch"){
+        document.getElementById('activation-selector').innerHTML = `
+        <option value="CELU">Celu</option>
+        <option value="ELU">Elu</option>
+        <option value="GELU">Gelu</option>
+        <option value="Hardshrink">Hard Shrink</option>
+        <option value="Hardsigmoid">Hard Sigmoid</option>
+        <option value="Hardtanh">Hard Tanh</option>
+        <option value="Hardswish">Hard Swish</option>
+        <option value="LeakyReLU">Leaky ReLU</option>
+        <option value="LogSigmoid">Log Sigmoid</option>
+        <option value="No Activation">No Activation</option>
+        <option value="PReLU">PReLU</option>
+        <option value="ReLU">ReLU</option>
+        <option value="ReLU6">ReLU 6</option>
+        <option value="RReLU">RReLU</option>
+        <option value="Sigmoid">Sigmoid</option>
+        <option value="SELU">Selu</option>
+        <option value="Softmax">Softmax</option>
+        <option value="Softplus">Softplus</option>
+        <option value="Softshrink">Softshrink</option>
+        <option value="Softsign">Softsign</option>
+        <option value="Tanh">Tanh</option>
+        <option value="Tanhshrink">Tanh Shrnik</option>
+        `;
+    }
+    document.getElementById("activation-selector").value = layer_config.activation;
 });
 
 (function() {
