78f829fd872eec7d2146ff6722f2756717b1047b
heidariarash <arashheidari94@outlook.com>
2020-10-23 16:35:20 +0330

    batch norm for pytorch added

---
+++ scripts/batch-norm-config.js
@@ -0,0 +1,27 @@
+const {ipcRenderer} = require('electron');
+let layer;
+
+document.getElementById('cancle').addEventListener('click', () => {
+    ipcRenderer.send('close-small');
+});
+
+document.getElementById('done').addEventListener('click', ()=> {
+    let complete = true;
+    if(document.getElementById("param_num").value < 1) { complete = false}
+    if(document.getElementById("param_num").value == "") { complete = false}
+    if( complete === false){
+        return;
+    }
+    layer.param_num = document.getElementById("param_num").value;
+    ipcRenderer.send("layer-config-finish", layer);
+});
+
+ipcRenderer.on("layer-config", (event , arg) => {
+    layer_config = arg.layer;
+    document.getElementById("param_num").value = layer_config.param_num;
+    layer = layer_config;
+});
+
+(function() {
+    ipcRenderer.send('ready-layer-config');
+ })();
